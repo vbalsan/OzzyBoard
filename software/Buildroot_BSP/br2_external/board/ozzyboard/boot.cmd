72aeb9d34a7c4fc5b09105e8098ffcaa777c0897
vbalsan <vincent.balsan@outlook.fr>
2023-10-21 00:17:04 -0400

    Added support for SPI, flashrom utility, and tweak to boot from SPI NOR

--- software/Buildroot_BSP/br2_external/board/ozzyboard/boot.cmd
+++ software/Buildroot_BSP/br2_external/board/ozzyboard/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk1p2 earlyprintk rw
-load mmc 1:1 0x41000000 zImage
-load mmc 1:1 0x41800000 Ozzyboard_kernel.dtb
+setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk0p2 earlyprintk rw
+load mmc 0:1 0x41000000 zImage
+load mmc 0:1 0x41800000 Ozzyboard_kernel.dtb
 bootz 0x41000000 - 0x41800000
